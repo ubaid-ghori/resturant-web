99e67379a94bbeb619adc8dfe67d991cc9e3c08a
ubaid ghori <ubaid.razaghori@gmail.com>
2024-05-24 18:37:37 +0500

    updated

--- src/app/Components/Homepage.jsx
+++ src/app/Components/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
      <div className='   Homepage  ' >
      </div>
 
-     <div className=' absolute'>
+     <div className=' absolute top-20 '>
         <Image src="/307cb4d4a609588fbd84352e4a8966e6.png"
         width={200}
         height={200}
